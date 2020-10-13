5ada13a0e6f5092fe7bf7d24f84743b14f7ea317
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-10-14 00:21:22 +0100

    converted simpleHttp from constructors to class's

--- simplehttpLibrary.js
+++ simplehttpLibrary.js
@@ -1,32 +1,71 @@
-function simpleHttp() {
-    this.xhr = new XMLHttpRequest();
-} 
+// function simpleHttp() {
+//     this.xhr = new XMLHttpRequest();
+// } 
 
-simpleHttp.prototype.get = function (url, callback ) {
-    setTimeout( () => {
+// simpleHttp.prototype.get = function (url, callback ) {
+//     setTimeout( () => {
 
-        this.xhr.open('GET', url, true);
+//         this.xhr.open('GET', url, true);
     
-        this.xhr.onload = () => {
-            if (this.xhr.status === 200) {
+//         this.xhr.onload = () => {
+//             if (this.xhr.status === 200) {
     
-                const resopnseObject = JSON.parse(this.xhr.responseText);
+//                 const resopnseObject = JSON.parse(this.xhr.responseText);
     
-                // console.log(resopnseObject);
-                callback(resopnseObject);
+//                 // console.log(resopnseObject);
+//                 callback(resopnseObject);
                 
-                document.getElementById('jokes').innerHTML = resopnseObject.value.joke;
+//                 document.getElementById('jokes').innerHTML = resopnseObject.value.joke;
 
-                return resopnseObject
+//                 // return resopnseObject
                 
-            }
+//             }
 
            
             
-        }
+//         }
         
         
-        this.xhr.send(null);
-    }, 3000);
+//         this.xhr.send(null);
+//     }, 3000);
+// }
+
+
+class simpleHttp {
+    constructor() {
+        this.xhr = new XMLHttpRequest();
+    }
+
+    get = function (url, callback) {
+        setTimeout( () => {
+
+            this.xhr.open('GET', url, true);
+        
+            this.xhr.onload = () => {
+                if (this.xhr.status === 200) {
+        
+                    const resopnseObject = JSON.parse(this.xhr.responseText);
+        
+                    // console.log(resopnseObject);
+                    callback(resopnseObject);
+                    
+                    document.getElementById('jokes').innerHTML = resopnseObject.value.joke;
+    
+                    // return resopnseObject
+                    
+                }
+    
+               
+                
+            }
+            
+            
+            this.xhr.send(null);
+        }, 3000);
+    }
 }
+
+
+
+
 
